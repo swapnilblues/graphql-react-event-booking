edde1bf64c0d04ef342d0c70c219e0bf688e8131
Swapnil Bhaumik <swapnilbhaumikcfc@gmail.com>
2020-06-02 04:58:28 -0400

    Added basic Read and Create items using graphql

--- app.js
+++ app.js
@@ -1,12 +1,34 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const { buildSchema } = require('graphql')
 
 const app = express()
 
 app.use(bodyParser.json())
 
-app.get('/', (req,res,next) => {
-    res.send('Hello World!')
-})
+app.use('/graphql',graphqlHttp({
+    schema: buildSchema(`
+        type RootQuery {
+            events: [String!]!
+        }
+        type RootMutation {
+            createEvent(name: String): String
+        }
+        schema {
+            query: RootQuery
+            mutation: RootMutation
+        }
+    `),
+    rootValue: {
+        events: () => {
+            return ['Ronaldo', 'Messi', 'Hazard', 'Werner']
+        },
+        createEvent: (args) => {
+            return args.name;
+        }
+    },
+    graphiql: true
+}))
 
 app.listen(3000)
